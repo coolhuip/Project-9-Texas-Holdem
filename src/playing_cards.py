278bea7ac9ca7efa0b8c913bce9cbed7efea618c
Danny <cool.huip@gmail.com>
2024-01-27 02:36:09 -0500

    Create new files

---
+++ src/playing_cards.py
@@ -0,0 +1,15 @@
+"""
+Classes related to playing cards.
+"""
+
+class Deck():
+    """
+    Sample Usage:
+    -------------
+    >>>
+    """
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
