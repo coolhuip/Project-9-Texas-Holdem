ac8c13e8eef99de7506d4f72af904c4f54cb1b97
Danny <cool.huip@gmail.com>
2024-02-02 03:54:27 -0500

    Set up template for main script

--- src/main.py
+++ src/main.py
@@ -1,5 +1,39 @@
 from __future__ import annotations
 from typing import Union, Optional, Any
+from playing_cards import Deck, Card
+
+"""
+Stages:
+-------
+1. Game Setup
+    - Minimum Bet
+    - Number of Players
+
+2. User Info
+    - Name
+    - Amount of Money
+
+3. Introduce Players
+
+4. Pre-Flop
+
+5. Flop
+
+6. Turn
+
+7. River
+
+8. Showdown (contingent)
+
+9. Display Results
+
+10. Distribute Winnings
+
+11. Kick Players w/ No Minimum Bet (contingent)
+
+12. Reset Round & Loop Back to (4)
+
+"""
 
 
 
@@ -7,8 +41,6 @@ from typing import Union, Optional, Any
 
 
 
-
-
-if __name__ '__main__':
+if __name__ == '__main__':
     import doctest
     doctest.testmod()
